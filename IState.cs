e82adf835b1b9892160e3a33796b4402622c573a
Amer Mamoun <amer.mn.96@gmail.com>
2019-04-30 15:33:51 +0200

    Add project files.

---
+++ IState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    /// <summary>
+    /// Interface to access the state level functions
+    /// </summary>
+    interface IState
+    {
+        /// <summary>
+        /// Checking if the current state is a goal state or not.
+        /// </summary>
+        /// <returns>Bool value that indicates if it is a goal state or not.</returns>
+        bool isGoal();
+
+        /// <summary>
+        /// Checking if the current state is valid
+        /// </summary>
+        /// <returns>Bool value that indicate that the state is valid or not.</returns>
+        bool isValid();
+        int Heuristics
+        {
+            get;
+        }
+    }
+}
---
+++ TicTacToeState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    class TicTacToeState : IState
+    {
+        int player = 1; // 1 or -1
+        int[,] board = new int[10, 10]; // game board
+        int x, y = 0; // postion of the last step
+        public bool isGoal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isValid()
+        {
+            throw new NotImplementedException();
+        }
+        public int Heuristics
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
